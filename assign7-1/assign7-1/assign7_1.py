74b90a7319fffaedf712114958c57b5e1978b22d
Jack He <34477981+hejk2008@users.noreply.github.com>
2018-06-05 19:11:33 +0800

    Update assign7

--- assign7-1/assign7-1/assign7_1.py
+++ assign7-1/assign7-1/assign7_1.py
@@ -1,1 +1,16 @@
+#! /usr/bin/env python3
+#coding:utf-8
 
+def main():
+    words = input('请输入两个单词，以空格分割：').split(sep=' ')
+    word1 = words[0]
+    word2 = words[1]
+    if set(word1) == set(word2):
+        print('单词%s与%s是相似词！'%(word1, word2))
+    else:
+        print('单词%s与%s不是相似词！'%(word1, word2))
+
+
+if __name__ == '__main__':
+    main()
+    
--- assign7-2/assign7-2/assign7_2.py
+++ assign7-2/assign7-2/assign7_2.py
@@ -1,1 +1,16 @@
+#! /usr/bin/env python3
+#coding:utf-8
 
+def main():
+    word = input('请输入：')
+    for i in range(len(word)-1,0,-1):
+        output = ' '.join((word[:i], word[i:]))
+        print(output)
+    print('-'*5)
+    for i in range(1,len(word)):
+        output = ' '.join((word[:i], word[i:]))
+        print(output)
+
+
+if __name__ == '__main__':
+    main()
