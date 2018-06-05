dd7e57720c51908508b51f497b29b2c059890fd7
Jack He <34477981+hejk2008@users.noreply.github.com>
2018-06-05 19:13:54 +0800

    Update assign8

--- assign8-1/assign8-1/assign8_1.py
+++ assign8-1/assign8-1/assign8_1.py
@@ -1,1 +1,32 @@
+#! /usr/bin/env python3
+#coding:utf-8
 
+import re
+
+def email_valid_check(addr):
+    if re.match(r'^\w+(\.\w+)*@\w+(\.\w+)*\.[a-zA-Z]+$',addr):
+        return True
+    else:
+        return False
+
+def email_regexp_test():
+    assert email_valid_check('jkhe17@fudan.edu.cn')
+    assert email_valid_check('jkhe17@fudanedu.cn')
+    assert email_valid_check('jkhe17@fudan_edu.com')
+    assert email_valid_check('jk.he17@fudan.edu.com')
+    assert not email_valid_check('jkhe17&fudan.edu.cn')
+    assert not email_valid_check('jkhe17@fudaneducn')
+    print('Self test passed!')
+
+
+def main():
+    email=input('Please input an email address:')
+    if email_valid_check(email) == True:
+        print('Is "%s" a valid email address? Ture'%email)
+    else:
+        print('Is "%s" a valid email address? False'%email)
+
+
+if __name__ == '__main__':
+    email_regexp_test()
+    main()
--- assign8-2/assign8-2/assign8_2.py
+++ assign8-2/assign8-2/assign8_2.py
@@ -1,1 +1,22 @@
+#! /usr/bin/env python3
+#coding:utf-8
 
+import re
+
+def rm_html_markup(s):
+    return re.sub(r'<.+?>','',s)
+
+    
+def reg_test():
+    assert rm_html_markup('<a href=”index.htm”>Welcome to Fudan University!</a>') == 'Welcome to Fudan University!'
+    print('Self test passed!')
+
+
+def main():
+    s = input('Please input a string with HTML markup:')
+    print(rm_html_markup(s))
+
+
+if __name__ == '__main__':
+    reg_test()
+    main()
