d7703e866a45b4d3c652028c421f59c0f7e6768b
Jack He <34477981+hejk2008@users.noreply.github.com>
2018-06-05 19:16:25 +0800

    Update assign9

--- assign9-1/assign9-1/assign9_1.py
+++ assign9-1/assign9-1/assign9_1.py
@@ -1,1 +1,12 @@
+#!  /usr/bin/env python3
+#coding:utf-8
 
+def isLeap(year):
+    '''Check if a year is leap year.'''
+    return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
+
+
+assert isLeap(2000) == True
+assert isLeap(2004) == True
+assert isLeap(2001) == False
+assert isLeap(2100) == False
--- assign9-2/assign9-2/assign9_2.py
+++ assign9-2/assign9-2/assign9_2.py
@@ -1,1 +1,17 @@
+#! /usr/bin/env python3
+#coding:utf-8
 
+def f1(s):
+    '''Transform a string to palindrome.'''
+    return s+s[-2::-1]
+
+
+def f2(s):
+    '''Check if a string is palindrome.'''
+    return s == s[::-1]
+
+
+assert f1('asddf') == 'asddfddsa'
+assert f1('a') == 'a'
+assert f2('asddf') == False
+assert f2('asddfddsa') == True
