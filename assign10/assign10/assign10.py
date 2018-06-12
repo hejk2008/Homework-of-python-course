8aa5d3783d3931ee422075c0bab5455f3293bc47
Jack He <34477981+hejk2008@users.noreply.github.com>
2018-06-12 19:43:06 +0800

    Update assign10

--- assign10/assign10/assign10.py
+++ assign10/assign10/assign10.py
@@ -1,1 +1,12 @@
+#! /usr/bin/env python3
+# coding: utf-8
 
+def main():
+    with open('sample1.txt','r') as f:
+        content = f.read()
+    content = content.upper()
+    with open('sample2.txt','w') as f:
+        f.write(content)
+
+if __name__ =='__main__':
+    main()
